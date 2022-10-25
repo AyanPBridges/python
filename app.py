5804381c18328aa7021db86a9099d491a7959b01
Ayan Ospanov <ospanovayan2003@gmail.com>
2022-10-25 23:50:51 +0600

    1comm

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import requests
+app = Flask(__name__)
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0',port=8080)
+
+def getInfo(network, address):
+    url = "https://solana-gateway.moralis.io/nft/" + network + "/" + address + "/metadata"
+    headers = {
+        "accept": "application/json",
+        "X-API-Key": "zUMibjOPQDAHs4wtYnhqwGNJUXGXAOwlNIO8VtajphV24cLA0xfTzxq3HRdOUHMx"
+    }
+    response = requests.get(url, headers=headers)
+    return response
+#example address = 8X6h5iqPik8FXP8kY3dDhEdMJjG2HUcmRfqoqGxQWXNc
+
+
+#netw = str(input("Network: "))
+#addr = str(input("Address: "))
+#print(getInfo(netw, addr))
---
+++ templates/index.js
@@ -0,0 +1,12 @@
+const serverUrl = "";
+const appId = "";
+Moralis.start({ serverUrl, appId });
+const getInfo = async () => {
+  const options = {
+    network: document.getElementById("network").value,
+    address: document.getElementById("address").value,};
+  const portfolio = await Moralis.SolanaAPI.account.getPortfolio(options);
+  console.log(portfolio);};
+document.getElementById("get-portfolio").onclick = getInfo();
+
+
